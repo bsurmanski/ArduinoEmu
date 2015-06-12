8bdae656005302cba9fdd5ece52988b48e5f807b
Brandon Surmanski <b.surmanski@gmail.com>
2015-06-12 10:24:49 -0400

    added Memory class. representation of Arduino memory

---
+++ Memory.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.IO;
+
+public class Memory {
+	byte[] data;
+
+	public Memory(int size) {
+		data = new byte[size];
+	}
+
+    public Memory(int size, Stream init) {
+        data = new byte[size];
+        init.Read(data, 0, size);
+    }
+}
